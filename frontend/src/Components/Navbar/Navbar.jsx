fa353b3ced79fdfa5a426f2b39f8b9e158f10af6
AnushanDharmarathna <haianushan@gmail.com>
2024-05-30 16:09:40 +0530

    pages are linked to the home page

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     const [menu,setMenu] = useState("home");
@@ -12,14 +13,14 @@ const Navbar = () => {
             <p>SHOES</p>
         </div>
         <ul className='nav-menu'>
-            <li onClick={()=>{setMenu("home")}}>Home{menu==="home"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("nike")}}>Nike{menu==="nike"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("addidas")}}>Addidas{menu==="addidas"?<hr/>:<></>}</li>
-            <li onClick={()=>{setMenu("reebok")}}>Reebok{menu==="reebok"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("home")}}><Link style={{textDecoration: 'none'}} to='/'>Home</Link>{menu==="home"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("nike")}}><Link style={{textDecoration: 'none'}} to='/nike'>Nike</Link>{menu==="nike"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("addidas")}}><Link style={{textDecoration: 'none'}} to='/addidas'>Addidas</Link>{menu==="addidas"?<hr/>:<></>}</li>
+            <li onClick={()=>{setMenu("reebok")}}><Link style={{textDecoration: 'none'}} to='/reebok'>Reebok</Link>{menu==="reebok"?<hr/>:<></>}</li>
         </ul>
         <div className='nav-login-cart'>
-            <button>Login</button>
-            <img src={cart_icon} alt='' />
+            <Link to='/login'><button>Login</button></Link>
+            <Link to='cart'><img src={cart_icon} alt='' /></Link>
             <div className='nav-cart-count'>0</div>
         </div>
     </div>
