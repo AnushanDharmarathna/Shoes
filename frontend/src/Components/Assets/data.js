b62c6fecc30ddbaafcb92f5184b8207387456360
AnushanDharmarathna <haianushan@gmail.com>
2024-06-05 01:42:49 +0530

    added a popular Nike shoes area on the home page

---
+++ frontend/src/Components/Assets/data.js
@@ -0,0 +1,37 @@
+import p1_img from './Product_1.png'
+import p2_img from './Product_2.png'
+import p3_img from './Product_3.png'
+import p4_img from './Product_4.png'
+
+let data_product = [
+    {
+        id:1,
+        name:"Sneakers Air Jordan Shoe Nike Air Max",
+        image:p1_img,
+        new_price:200.00,
+        old_price:220.00,
+    },
+    {
+        id:2,
+        name:"Nike Air Max Nike Free Air Force Shoe",
+        image:p2_img,
+        new_price:100.00,
+        old_price:120.00,
+    },
+    {
+        id:3,
+        name:"Shoe Nike Air Max Sneakers Running, running shoes",
+        image:p3_img,
+        new_price:100.00,
+        old_price:120.00,
+    },
+    {
+        id:4,
+        name:"Nike Free Nike Air Max Sneakers Shoe, red shoes",
+        image:p4_img,
+        new_price:150.00,
+        old_price:180.00,
+    }
+];
+
+export default data_product;
