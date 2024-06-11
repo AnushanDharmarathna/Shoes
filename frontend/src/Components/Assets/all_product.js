ae77c022b9ce1d7c392d62969e128e5014581925
AnushanDharmarathna <haianushan@gmail.com>
2024-06-12 02:59:53 +0530

    add more assets images and create all product array

---
+++ frontend/src/Components/Assets/all_product.js
@@ -0,0 +1,295 @@
+import p1_img from './Ni_Product_1.png'
+import p2_img from './Ni_Product_2.png'
+import p3_img from './Ni_Product_3.png'
+import p4_img from './Ni_Product_4.png'
+import p5_img from './Ni_Product_5.png'
+import p6_img from './Ni_Product_6.png'
+import p7_img from './Ni_Product_7.png'
+import p8_img from './Ni_Product_8.png'
+import p9_img from './Ni_Product_9.png'
+import p10_img from './Ni_Product_10.png'
+import p11_img from './Ni_Product_11.png'
+import p12_img from './Ni_Product_12.png'
+
+import p13_img from './Ad_Product_1.png'
+import p14_img from './Ad_Product_2.png'
+import p15_img from './Ad_Product_3.png'
+import p16_img from './Ad_Product_4.png'
+import p17_img from './Ad_Product_5.png'
+import p18_img from './Ad_Product_6.png'
+import p19_img from './Ad_Product_7.png'
+import p20_img from './Ad_Product_8.png'
+import p21_img from './Ad_Product_9.png'
+import p22_img from './Ad_Product_10.png'
+import p23_img from './Ad_Product_11.png'
+import p24_img from './Ad_Product_12.png'
+
+import p25_img from './Re_Product_1.png'
+import p26_img from './Re_Product_2.png'
+import p27_img from './Re_Product_3.png'
+import p28_img from './Re_Product_4.png'
+import p29_img from './Re_Product_5.png'
+import p30_img from './Re_Product_6.png'
+import p31_img from './Re_Product_7.png'
+import p32_img from './Re_Product_8.png'
+import p33_img from './Re_Product_9.png'
+import p34_img from './Re_Product_10.png'
+import p35_img from './Re_Product_11.png'
+import p36_img from './Re_Product_12.png'
+
+let all_product = [
+    {
+        id:1,
+        name:"Air Jordan Shoe Nike Air Max",
+        image:p1_img,
+        new_price:200.00,
+        old_price:220.00,
+    },
+    {
+        id:2,
+        name:"Nike Air Max Nike Free Air Force Shoe",
+        image:p2_img,
+        new_price:100.00,
+        old_price:120.00,
+    },
+    {
+        id:3,
+        name:"Shoe Nike Air Max Sneakers Running, running shoes",
+        image:p3_img,
+        new_price:100.00,
+        old_price:120.00,
+    },
+    {
+        id:4,
+        name:"Nike Free Nike Air Max Sneakers Shoe, red shoes",
+        image:p4_img,
+        new_price:150.00,
+        old_price:180.00,
+    },
+    {
+        id:5,
+        name:"Nike Air Jordan 1 Mid shoes",
+        image:p5_img,
+        new_price:250.00,
+        old_price:280.00,
+    },
+    {
+        id:6,
+        name:"Nike Womens Air Zoom",
+        image:p6_img,
+        new_price:150.00,
+        old_price:170.00,
+    },
+    {
+        id:7,
+        name:"Nike AF1 Air Force One",
+        image:p7_img,
+        new_price:140.00,
+        old_price:160.00,
+    },
+    {
+        id:8,
+        name:"Nike Womens Air Zoom",
+        image:p8_img,
+        new_price:150.00,
+        old_price:170.00,
+    },
+    {
+        id:9,
+        name:"Nike Free Run 2 Hyper Cobalt Blue",
+        image:p9_img,
+        new_price:80.00,
+        old_price:100.00,
+    },
+    {
+        id:10,
+        name:"Nike Air Max 2017 “Deep Royal Blue”",
+        image:p10_img,
+        new_price:250.00,
+        old_price:260.00,
+    },
+    {
+        id:11,
+        name:"Nike Patike Nike Zoom Vapor 9.5 Tour",
+        image:p11_img,
+        new_price:200.00,
+        old_price:220.00,
+    },
+    {
+        id:12,
+        name:"Air Jordan 1 Retro High Flex PS 'Varsity Red'",
+        image:p12_img,
+        new_price:300.00,
+        old_price:320.00,
+    },
+    {
+        id:13,
+        name:"Adidas NMD_R1 - Stadium Goods",
+        image:p13_img,
+        new_price:150.00,
+        old_price:180.00,
+    },
+    {
+        id:14,
+        name:"Adidas Ultra Boost 2.0 Mineral Green",
+        image:p14_img,
+        new_price:180.00,
+        old_price:200.00,
+    },
+    {
+        id:15,
+        name:"Adidas Women's Barricade",
+        image:p15_img,
+        new_price:140.00,
+        old_price:150.00,
+    },
+    {
+        id:16,
+        name:"Adidas Superstar II C White Black Leather",
+        image:p16_img,
+        new_price:100.00,
+        old_price:120.00,
+    },
+    {
+        id:17,
+        name:"Adidas Predito LZ TRX FG Soccer Cleat",
+        image:p17_img,
+        new_price:200.00,
+        old_price:220.00,
+    },
+    {
+        id:18,
+        name:"Adidas Men's Stabil S Indoor Court Shoe",
+        image:p18_img,
+        new_price:130.00,
+        old_price:140.00,
+    },
+    {
+        id:19,
+        name:"Adidas Stella McCartney Dorifera",
+        image:p19_img,
+        new_price:250.00,
+        old_price:280.00,
+    },
+    {
+        id:20,
+        name:"Adidas Superstar 80s",
+        image:p20_img,
+        new_price:70.00,
+        old_price:90.00,
+    },
+    {
+        id:21,
+        name:"Adidas F50 Adizero TRX FG",
+        image:p21_img,
+        new_price:130.00,
+        old_price:140.00,
+    },
+    {
+        id:22,
+        name:"Adidas Tubular Radial Men's",
+        image:p22_img,
+        new_price:280.00,
+        old_price:300.00,
+    },
+    {
+        id:23,
+        name:"Adidas Nmd Trail Pk White Mountaineering Navy Red White",
+        image:p23_img,
+        new_price:150.00,
+        old_price:160.00,
+    },
+    {
+        id:24,
+        name:"Adidas Munchen Navy Blue",
+        image:p24_img,
+        new_price:80.00,
+        old_price:100.00,
+    },
+    {
+        id:25,
+        name:"Reebok Classic Leather Black",
+        image:p25_img,
+        new_price:200.00,
+        old_price:220.00,
+    },
+    {
+        id:26,
+        name:"Reebok Exofit Lo Clean Logo Trainers White",
+        image:p26_img,
+        new_price:80.00,
+        old_price:100.00,
+    },
+    {
+        id:27,
+        name:"Men's Reebok KL x Classic Leather 'Perfect Split",
+        image:p27_img,
+        new_price:50.00,
+        old_price:60.00,
+    },
+    {
+        id:28,
+        name:"Reebok Pump Plus Tech EU 43",
+        image:p28_img,
+        new_price:100.00,
+        old_price:120.00,
+    },
+    {
+        id:29,
+        name:"Reebok Pump Omni Lite Dee Brown",
+        image:p29_img,
+        new_price:80.00,
+        old_price:100.00,
+    },
+    {
+        id:30,
+        name:"Reebok Crossfit Nano 8.0 Flexweave",
+        image:p30_img,
+        new_price:50.00,
+        old_price:70.00,
+    },
+    {
+        id:31,
+        name:"Reebok Pump Omni Lite Solebox Grey Pink",
+        image:p31_img,
+        new_price:120.00,
+        old_price:140.00,
+    },
+    {
+        id:32,
+        name:"Reebok Club C 85 Tonal Gum Hunter Green",
+        image:p32_img,
+        new_price:150.00,
+        old_price:160.00,
+    },
+    {
+        id:33,
+        name:"Reebok Womens Blue Mesh Work Shoes Steel Toe Athletic Oxfords",
+        image:p33_img,
+        new_price:40.00,
+        old_price:50.00,
+    },
+    {
+        id:34,
+        name:"Reebok Classic Leather Deconstructed",
+        image:p34_img,
+        new_price:130.00,
+        old_price:140.00,
+    },
+    {
+        id:35,
+        name:"Reebok Shaq Attaq",
+        image:p35_img,
+        new_price:130.00,
+        old_price:150.00,
+    },
+    {
+        id:36,
+        name:"Men's Fila Orbit Low Shoes",
+        image:p36_img,
+        new_price:200.00,
+        old_price:210.00,
+    }
+];
+
+export default all_product;
