a3ea40f563786f4ac484f15ca894d9cdfbe040dc
AnushanDharmarathna <haianushan@gmail.com>
2024-08-09 16:11:50 +0530

    modify pages banners

--- frontend/src/Pages/HomeCategory.jsx
+++ frontend/src/Pages/HomeCategory.jsx
@@ -6,7 +6,17 @@ const HomeCategory = (props) => {
   const {all_product} = useContext(HomeContext);
   return (
     <div className="home-category">
-      <img src={props.banner} alt='' />
+      <div className="banner">
+        <div className="banner-left">
+          <h1>FLAT 50% OFF</h1>
+          <h2>12 Hours 20 Mins</h2>
+          <button>Explore Now</button>
+        </div>
+        <div className="banner-right">
+          <img src={props.banner} alt='' />
+        </div>
+      </div>
+      
     </div>
   )
 }
