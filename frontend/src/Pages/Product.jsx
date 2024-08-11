a20c720cfe37da56d2d595528cdbc46927edb674
AnushanDharmarathna <haianushan@gmail.com>
2024-08-11 15:48:04 +0530

    make product display page

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { useParams } from 'react-router-dom';
 import {HomeContext} from '../Context/HomeContext'
 import Breadcrums from '../Components/Breadcrums/Breadcrums';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
 const Product = () => {
   const {all_product}= useContext(HomeContext);
@@ -10,6 +11,7 @@ const Product = () => {
   return (
     <div>
       <Breadcrums product={product}/>
+      <ProductDisplay product={product} />
     </div>
   )
 }
