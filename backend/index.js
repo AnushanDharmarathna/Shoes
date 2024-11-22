64594e2fa893f07c8288631797751171168101c5
Anushan Dharmarathna <139634835+AnushanDharmarathna@users.noreply.github.com>
2024-11-22 16:23:10 +0530

    image upload API

---
+++ backend/index.js
@@ -0,0 +1,85 @@
+const port = 4000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+const { type } = require("os");
+
+app.use(express.json());
+app.use(cors());
+
+// Database
+mongoose.connect("mongodb+srv://admin:admin12345@cluster0.gxlg3.mongodb.net/shoes");
+
+// API
+app.get("/",(req,res)=>{
+    res.send("Express App is running")
+})
+
+//Image Storage
+const Storage = multer.diskStorage({
+    destination: './upload/images',
+    filename:(req,file,cb)=>{
+        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({storage:Storage})
+
+//Image upload endpoint
+app.use("/images",express.static("upload/images"))
+
+app.post("/upload",upload.single('product'),(req,res)=>{
+    res.json({
+        success:1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+//DB schema for product
+const Product = mongoose.model("Product",{
+    id:{
+        type: Number,
+        required:true,
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    new_price:{
+        type:Number,
+        required:true,
+    },
+    old_price:{
+        type:Number,
+        required:true,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    },
+    avilable:{
+        type:Boolean,
+        default:true,
+    }
+})
+
+app.listen(port,(error)=>{
+    if (!error) {
+        console.log("Server is runing on Port "+port)
+    }
+    else{
+        console.log("error : "+error)
+    }
+})
